0cc86fe210bdef271b036ef4c1024ba40671db1c
aaronluannguyen <aaronluannguyen@gmail.com>
2018-09-28 03:44:16 -0700

    Finished Chat App card

--- src/Projects/ChatApp/ChatAppCard.js
+++ src/Projects/ChatApp/ChatAppCard.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './ChatAppCard.css';
 import Logo from "../../assets/Projects/ChatApp/CardCover.png";
+import backLogo from '../../assets/Projects/ChatApp/backLogo.svg';
+import BulletPoint from "../../Components/BulletPoint/BulletPoint";
+import ViewProject from "../../Components/ViewProject/ViewProject";
+import {ROUTES} from "../../constants";
 
 
 export default class ChatAppCard extends React.Component {
@@ -14,7 +18,20 @@ export default class ChatAppCard extends React.Component {
         {
           this.props.hover ?
             <div className="onHoverContainer-ChatAppCard">
-              LOL
+              <img src={backLogo} alt="Slack Rebuilt Logo" className='backLogo-ChatAppCard'/>
+              <div className="PositionTimelineContainer-ChatAppCard">
+                <div className="Position-ChatAppCard">
+                  Full Stack Developer
+                </div>
+                <div className="Timeline-ChatAppCard">
+                  January 2018 - June 2018
+                </div>
+              </div>
+              <BulletPoint bulletContent="I built a full stack chat application throughout my Frontend and
+              Server-Side Development courses. I built the frontend of my chat application using Facebook's
+              React library. The backend was developed using tools like Google's Go language, Node microservices,
+              Docker Containers, and Digital Ocean Droplets."/>
+              <ViewProject history={this.props.history} projectPage={ROUTES.ChatApp}/>
             </div>
             :
             <img src={Logo} alt="chat app example" className="logo-ChatAppCard"/>
