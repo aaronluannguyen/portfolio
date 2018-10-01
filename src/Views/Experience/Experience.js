5a7b5055c401d43de62cf47df10dda0cad089ed3
aaronluannguyen <aaronluannguyen@gmail.com>
2018-10-01 01:48:54 -0700

    Added project section to experience view

--- src/Views/Experience/Experience.js
+++ src/Views/Experience/Experience.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Experience.css';
+import ProjectCard from "../../Components/ProjectCard/ProjectCard";
+import {PROJECTS} from "../../constants";
 
 
 export default class ExperiencePage extends React.Component {
@@ -9,8 +11,39 @@ export default class ExperiencePage extends React.Component {
 
   render() {
     return (
-      <div className='container-Experience'>
-        EXPERIENCE
+      <div className='container-ExperienceView'>
+        <div className='skillsContainer-ExperienceView'>
+          <div className='skillsTitle-ExperienceView'>
+            What can I do?
+          </div>
+          <div className='skillsDetailsContainer-ExperienceView'>
+            <div>
+
+            </div>
+          </div>
+        </div>
+        <div className='projectSection-ExperienceView'>
+          <div className='projectTitle-ExperienceView'>
+            Projects
+          </div>
+          <div className='projects-ExperienceView'>
+            <div className="ProjectCard-ExperienceView">
+              <ProjectCard history={this.props.history} project={PROJECTS.PeekaVR}/>
+            </div>
+            <div className="ProjectCard-ExperienceView">
+              <ProjectCard history={this.props.history} project={PROJECTS.EcoSnap}/>
+            </div>
+            <div className="ProjectCard-ExperienceView">
+              <ProjectCard history={this.props.history} project={PROJECTS.ChatApp}/>
+            </div>
+            <div className="ProjectCard-ExperienceView">
+              <ProjectCard history={this.props.history} project={PROJECTS.Databases}/>
+            </div>
+            <div className="ProjectCard-ExperienceView">
+              <ProjectCard history={this.props.history} project={PROJECTS.Databases}/>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
